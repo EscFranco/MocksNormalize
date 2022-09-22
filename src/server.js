d8a463d9495c37a9374aca03eb6f820309f66aa4
Franco Escobar <escobarfranco95@gmail.com>
2022-09-22 00:23:06 -0300

    NGINX

--- src/server.js
+++ src/server.js
@@ -13,14 +13,6 @@ app.use(express.urlencoded({ extended: true }))
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers'
-const { PORT }  = yargs(hideBin(process.argv)).alias({p: 'PORT'}).default({PORT: 8080}).argv;
-
-// ------------------------- SOCKET.IO ------------------------- //
-
-import { Server } from 'socket.io';
-import http from 'http'
-const server = http.createServer(app);
-const io = new Server(server);
 
 // ------------------------- MONGODB ------------------------- //
 
@@ -34,25 +26,9 @@ mongoose.connect(config.MONGO_URL, { useNewUrlParser: true })
 
 const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true }
 
-// ------------------------- AUTH  ------------------------- //
-
-const isAuth = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        res.redirect('/login');
-    }
-};
-
-const isLogged = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        next();
-    } else {
-        res.redirect('/content');
-    }
-};
-
 // ------------------------- LOGIN ------------------------- //
+
+import { isAuth, isLogged } from './auth.js'
 
 app.use(session({
     store: MongoStore.create({
@@ -70,42 +46,42 @@ app.use(session({
 }))
 
 app.use(passport.initialize());
-app.use(passport.session());
+    app.use(passport.session());
 
 
-app.get('/', isAuth, (req, res) => {
-    res.redirect('/content');
-});
+    app.get('/', isAuth, (req, res) => {
+        res.redirect('/content');
+    });
 
-app.get('/login', isLogged, (req, res) => {
-    res.sendFile(path.resolve('public', 'login.html'));
-});
+    app.get('/login', isLogged, (req, res) => {
+        res.sendFile(path.resolve('public', 'login.html'));
+    });
 
-app.get('/register', isLogged, (req, res) => {
-    res.sendFile(path.resolve('public', 'register.html'));
-});
+    app.get('/register', isLogged, (req, res) => {
+        res.sendFile(path.resolve('public', 'register.html'));
+    });
 
-app.get('/failRegister', (req, res) => {
-    res.sendFile(path.resolve('public/failRegister.html'));
-});
+    app.get('/failRegister', (req, res) => {
+        res.sendFile(path.resolve('public/failRegister.html'));
+    });
 
-app.get('/failLogin', (req, res) => {
-    res.sendFile(path.resolve('public/failLogin.html'));
-});
+    app.get('/failLogin', (req, res) => {
+        res.sendFile(path.resolve('public/failLogin.html'));
+    });
 
-app.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (!err) res.redirect('/login')
-        else res.send({ status: 'Logout Error', body: err })
-    })
-});
+    app.get('/logout', (req, res) => {
+        req.session.destroy(err => {
+            if (!err) res.redirect('/login')
+            else res.send({ status: 'Logout Error', body: err })
+        })
+    });
 
-app.get('/username', (req, res) => {
-    res.json(`Bienvenido a la pagina ${req.session.passport.user}`)
-});
+    app.get('/username', (req, res) => {
+        res.json(`Bienvenido a la pagina ${req.session.passport.user}`)
+    });
 
-app.use('/content/*', isAuth);
-app.use('/content', express.static('public'));
+    app.use('/content/*', isAuth);
+    app.use('/content', express.static('public'));
 
 // ------------------------- RUTAS  ------------------------- //
 
@@ -117,8 +93,6 @@ app.use('/api/productos-test', routerProductos)
 app.use('/api/randoms', routerNumeros)
 app.use('/', routerAuth)
 
-// ------------------------- PROCESS  ------------------------- //
-
 app.get('/info', (req, res) => {
 
     res.json({
@@ -128,14 +102,19 @@ app.get('/info', (req, res) => {
         MemoriaReservada : process.memoryUsage().rss,
         ExecPath : process.execPath,
         ProcessID : process.pid,
-        ProyectFolder : process.cwd()
+        ProyectFolder : process.cwd(),
+        ProcesadoresPresentes: numCPUs,
     })
 });
 
-// ------------------------- CHAT ------------------------- //
 
+// ------------------------- SOCKET.IO ------------------------- //
+
+import { Server } from 'socket.io';
+import http from 'http'
 import { ApiMensajesFS } from './api/mensajes.js';
-
+const server = http.createServer(app);
+const io = new Server(server);
 const chat = new ApiMensajesFS()
 
 io.on('connection', async function (socket) {
@@ -143,16 +122,35 @@ io.on('connection', async function (socket) {
     socket.emit('mensaje', mensajes)
 
     socket.on('new-message', async function (objeto) {
-        console.log(objeto)
         await chat.newMensaje(objeto)
         let mensajes = await chat.getAll()
         io.sockets.emit('mensaje', mensajes); //emitir todos los mensajes a todos los clientes
     });
 });
 
-// ------------------------- SERVER ------------------------- //
 
-const srv = server.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
-})
-srv.on("error", error => console.log(`Error en servidor ${error}`))
+// ------------------------- CLUSTER/FORK ------------------------- //
+
+import cluster from 'cluster';
+import os from 'os'
+const numCPUs = os.cpus().length
+
+if (cluster.isPrimary) {
+    console.log(`PID MASTER ${process.pid}`)
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+    cluster.on('exit', worker => {
+        console.log('Worker', worker.process.pid, 'died', new Date().toLocaleString())
+        cluster.fork()
+    })
+} else {
+    const { PORT } = yargs(hideBin(process.argv)).alias({ p: 'PORT' }).default({ PORT: 8080 }).argv;
+
+    const srv = server.listen(PORT, () => {
+        console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+    })
+    srv.on("error", error => console.log(`Error en servidor ${error}`))
+    
+}
+
